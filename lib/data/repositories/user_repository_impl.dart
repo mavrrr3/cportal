2f6dbbe94379a459795ec3972fb6f875c30b1616
isoniasid <isoniasid@gmail.com>
2022-03-31 15:51:25 +0300

    рлзвл проверку интернет соединения до запроса

--- lib/data/repositories/user_repository_impl.dart
+++ lib/data/repositories/user_repository_impl.dart
@@ -1,5 +1,7 @@
 import 'package:cportal_flutter/core/error/exception.dart';
+import 'package:cportal_flutter/core/platform/network_info.dart';
 import 'package:cportal_flutter/data/datasources/user_remote_datasource.dart';
+import 'package:cportal_flutter/data/models/user_model.dart';
 import 'package:cportal_flutter/domain/entities/user_entity.dart';
 import 'package:cportal_flutter/core/error/failure.dart';
 import 'package:cportal_flutter/domain/repositories/user_repository.dart';
@@ -7,16 +9,42 @@ import 'package:dartz/dartz.dart';
 
 class UserRepositoryImpl implements UserRepository {
   final UserRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
 
-  UserRepositoryImpl({required this.remoteDataSource});
+  UserRepositoryImpl({
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+
   @override
   Future<Either<Failure, UserEntity>> getSingleUser(String id) async {
-    try {
-      final remoteUser = await remoteDataSource.getSingleUser(id);
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteUser = await remoteDataSource.getSingleUser(id);
 
-      return Right(remoteUser);
-    } on ServerException {
-      return Left(ServerFailure());
+        return Right(remoteUser);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        //TODO Реализовать обращение к локальным данным в случае отсутствия интернет соединения
+        const localeUser = UserModel(
+          id: 'id',
+          username: 'Изкэш Изкэшевич Изкешев',
+          position: 'Мастер цеха №5',
+          image:
+              'https://demotivation.ru/wp-content/uploads/2021/01/scale_1200-1-768x403.jpg',
+          department: 'Бухгалтерия',
+          email: 'email@mail.ru',
+          internalPhone: '43528',
+          externalPhone: '+79997373838',
+        );
+
+        return const Right(localeUser);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
     }
   }
 
