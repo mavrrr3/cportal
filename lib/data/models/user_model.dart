a660b7105059a0e61e1044f1880f11ef59406da3
isoniasid <isoniasid@gmail.com>
2022-04-03 15:45:16 +0300

    ПРИВЁЛ UserEntity к структуре БД

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,23 +1,91 @@
+// Для парсинга JSON данных
+//
+// final userEntity = userEntityFromJson(jsonString);
+
+import 'dart:convert';
 import 'package:cportal_flutter/domain/entities/user_entity.dart';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
 
 class UserModel extends UserEntity {
   const UserModel({
-    required String id,
-    required String username,
-    required String position,
-    required String image,
-    required String department,
-    required String email,
-    required String internalPhone,
-    required String externalPhone,
+    required final String id,
+    required final String userName,
+    required final String profileId,
+    required final DateTime lastLogin,
+    required final bool blocked,
+    required final DateTime dateCreated,
+    required final String userCreated,
+    required final DateTime? dateUpdated,
+    required final String userUpdated,
+    required final UserTypeModel userType,
   }) : super(
           id: id,
-          username: username,
-          position: position,
-          image: image,
-          department: department,
-          email: email,
-          internalPhone: internalPhone,
-          externalPhone: externalPhone,
+          userName: userName,
+          profileId: profileId,
+          lastLogin: lastLogin,
+          blocked: blocked,
+          dateCreated: dateCreated,
+          userCreated: userCreated,
+          dateUpdated: dateUpdated,
+          userUpdated: userUpdated,
+          userType: userType,
         );
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json['id'],
+        userName: json['user_name'],
+        profileId: json['profile_id'],
+        lastLogin: DateTime.parse(json['last_login']),
+        blocked: json['blocked'],
+        dateCreated: DateTime.parse(json['date_created']),
+        userCreated: json['user_created'],
+        dateUpdated: json['date_updated'],
+        userUpdated: json['user_updated'],
+        userType: UserTypeModel.fromJson(json['user_type']),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'user_name': userName,
+        'profile_id': profileId,
+        'last_login': lastLogin.toIso8601String(),
+        'blocked': blocked,
+        'date_created': dateCreated.toIso8601String(),
+        'user_created': userCreated,
+        'date_updated': dateUpdated,
+        'user_updated': userUpdated,
+        'user_type': (userType as UserTypeModel).toRawJson(),
+      };
+}
+
+class UserTypeModel extends UserTypeEntity {
+  UserTypeModel({
+    required final String id,
+    required final String code,
+    required final String description,
+  }) : super(
+          id: id,
+          code: code,
+          description: description,
+        );
+
+  factory UserTypeModel.fromRawJson(String str) =>
+      UserTypeModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory UserTypeModel.fromJson(Map<String, dynamic> json) => UserTypeModel(
+        id: json['id'],
+        code: json['code'],
+        description: json['description'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'code': code,
+        'description': description,
+      };
 }
--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -1,35 +1,54 @@
 import 'package:equatable/equatable.dart';
+import 'dart:convert';
 
 class UserEntity extends Equatable {
-  final String id;
-  final String username;
-  final String position;
-  final String image;
-  final String department;
-  final String email;
-  final String internalPhone;
-  final String externalPhone;
-
   const UserEntity({
     required this.id,
-    required this.username,
-    required this.position,
-    required this.image,
-    required this.department,
-    required this.email,
-    required this.internalPhone,
-    required this.externalPhone,
+    required this.userName,
+    required this.profileId,
+    required this.lastLogin,
+    required this.blocked,
+    required this.dateCreated,
+    required this.userCreated,
+    required this.dateUpdated,
+    required this.userUpdated,
+    required this.userType,
   });
+
+  final String id;
+  final String userName;
+  final String profileId;
+  final DateTime lastLogin;
+  final bool blocked;
+  final DateTime dateCreated;
+  final String userCreated;
+  final DateTime? dateUpdated;
+  final String userUpdated;
+  final UserTypeEntity userType;
 
   @override
   List<Object?> get props => [
         id,
-        username,
-        position,
-        image,
-        department,
-        email,
-        internalPhone,
-        externalPhone,
+        userName,
+        profileId,
+        lastLogin,
+        blocked,
+        dateCreated,
+        userCreated,
+        dateUpdated,
+        userUpdated,
+        userType,
       ];
+}
+
+class UserTypeEntity {
+  UserTypeEntity({
+    required this.id,
+    required this.code,
+    required this.description,
+  });
+
+  final String id;
+  final String code;
+  final String description;
 }
