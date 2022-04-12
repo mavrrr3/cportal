8b060313462a28a10207ddb4cd01650611e8a2f3
isoniasid <isoniasid@gmail.com>
2022-04-12 22:48:00 +0300

    РФКТРудалтл лишний файл

--- lib/presentation/ui/pages/pin_code_page.dart
+++ lib/presentation/ui/pages/pin_code_page.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:cportal_flutter/presentation/go_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
