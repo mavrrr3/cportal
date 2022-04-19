6b0de74e2940d686d5095868a991d0d62550e686
isoniasid <isoniasid@gmail.com>
2022-04-19 23:19:19 +0300

    РЛЗВЛ логику смены цвета в bottomNavBar

--- lib/feature/presentation/ui/home/home_page.dart
+++ lib/feature/presentation/ui/home/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
+
 import 'package:cportal_flutter/common/app_colors.dart';
 import 'package:cportal_flutter/common/theme.dart';
 import 'package:cportal_flutter/feature/presentation/ui/main_page/main_page.dart';
 import 'package:cportal_flutter/feature/presentation/ui/main_page/widgets/svg_icon.dart';
+import 'package:cportal_flutter/feature/presentation/ui/news_page/news_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -17,102 +20,117 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    List<Widget> listPages = <Widget>[
+    final Color _nonActiveColor = AppColors.kLightTextColor.withOpacity(0.48);
+    const Color _activeColor = AppColors.blueIcon;
+
+    final double _width = MediaQuery.of(context).size.width;
+
+    // Список страниц для навигации должен
+    // строго соответствовать количеству элемнтов навбара
+    List<Widget> _listPages = <Widget>[
+      const MainPage(),
+      const NewsPage(),
+      const MainPage(),
+      const NewsPage(),
       const MainPage(),
     ];
 
+    Color _iconColor(int index) {
+      return _selectedItemIndex == index ? _activeColor : _nonActiveColor;
+    }
+
+    GestureDetector _navBarItem({
+      required int index,
+      required double width,
+      required Widget iconWidget,
+      required String text,
+    }) {
+      return GestureDetector(
+        onTap: () => setState(
+          () => _selectedItemIndex = index,
+        ),
+        child: Container(
+          height: 56.h,
+          width: width / 5,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 5.h),
+              iconWidget,
+              SizedBox(height: 5.h),
+              Text(
+                text,
+                style: kMainTextInter.copyWith(
+                  fontSize: 9.sp,
+                  color: _iconColor(index),
+                ),
+              ),
+              SizedBox(height: 5.h),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
-      body: listPages[_selectedItemIndex],
-      bottomNavigationBar: Container(
-        color: AppColors.blue,
+      body: _listPages[_selectedItemIndex],
+      bottomNavigationBar: SizedBox(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            NavBarItem(
+            _navBarItem(
               index: 0,
-              width: width,
-              iconWidget: SvgIcon(null, path: 'navbar/main.svg', width: 24.w),
+              width: _width,
+              iconWidget: SvgIcon(
+                _iconColor(0),
+                path: 'navbar/main.svg',
+                width: 24.w,
+              ),
               text: 'Главная',
             ),
-            NavBarItem(
+            _navBarItem(
               index: 1,
-              width: width,
-              iconWidget: SvgIcon(null, path: 'navbar/news.svg', width: 20.w),
+              width: _width,
+              iconWidget: SvgIcon(
+                _iconColor(1),
+                path: 'navbar/news.svg',
+                width: 20.w,
+              ),
               text: 'Новости',
             ),
-            NavBarItem(
+            _navBarItem(
               index: 2,
-              width: width,
-              iconWidget:
-                  SvgIcon(null, path: 'navbar/questions.svg', width: 22.w),
+              width: _width,
+              iconWidget: SvgIcon(
+                _iconColor(2),
+                path: 'navbar/questions.svg',
+                width: 22.w,
+              ),
               text: 'Вопросы',
             ),
-            NavBarItem(
+            _navBarItem(
               index: 3,
-              width: width,
-              iconWidget:
-                  SvgIcon(null, path: 'navbar/declaration.svg', width: 22.0.w),
+              width: _width,
+              iconWidget: SvgIcon(
+                _iconColor(3),
+                path: 'navbar/declaration.svg',
+                width: 22.0.w,
+              ),
               text: 'Заявки',
             ),
-            NavBarItem(
+            _navBarItem(
               index: 4,
-              width: width,
-              iconWidget:
-                  SvgIcon(null, path: 'navbar/contacts.svg', width: 20.0.w),
+              width: _width,
+              iconWidget: SvgIcon(
+                _iconColor(4),
+                path: 'navbar/contacts.svg',
+                width: 20.0.w,
+              ),
               text: 'Контакты',
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NavBarItem extends StatefulWidget {
-  final int index;
-  final Widget iconWidget;
-  final double width;
-
-  final String text;
-  const NavBarItem({
-    Key? key,
-    required this.index,
-    required this.width,
-    required this.iconWidget,
-    required this.text,
-  }) : super(key: key);
-
-  @override
-  State<NavBarItem> createState() => _NavBarItemState();
-}
-
-class _NavBarItemState extends State<NavBarItem> {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedItemIndex = widget.index;
-        });
-      },
-      child: Container(
-        height: 56.h,
-        width: widget.width / 5,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 5.h),
-            widget.iconWidget,
-            SizedBox(height: 5.h),
-            Text(
-              widget.text,
-              style: kMainTextInter.copyWith(fontSize: 9.sp),
-            ),
-            SizedBox(height: 5.h),
           ],
         ),
       ),
