9eca9f8aed28d0db2ab4bea09e66ddd5c29bf38a
isoniasid <isoniasid@gmail.com>
2022-04-20 20:18:36 +0300

    добавил поля birthday и department

--- lib/feature/data/datasources/profile_datasource/profile_local_datasource.dart
+++ lib/feature/data/datasources/profile_datasource/profile_local_datasource.dart
@@ -20,7 +20,7 @@ class ProfileLocalDataSource implements IProfileLocalDataSource {
   @override
   Future<void> singleProfileToCache(ProfileModel profile) async {
     // await Hive.deleteBoxFromDisk('single_profile');
-
+    log(profile.toString());
     var box = await Hive.openBox<ProfileModel>('single_profile');
 
     await box.put(profile.id, profile);
