81a2e48dbcee59a1c2b779e5666f279820a4b2e7
isoniasid <isoniasid@gmail.com>
2022-04-20 20:21:45 +0300

    частично реализовал стрраницу Профиль

--- lib/feature/presentation/ui/profile/profile_page.dart
+++ lib/feature/presentation/ui/profile/profile_page.dart
@@ -1,9 +1,13 @@
+import 'package:cportal_flutter/common/app_colors.dart';
+import 'package:cportal_flutter/common/theme.dart';
 import 'package:cportal_flutter/feature/domain/entities/profile_entity.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/user_bloc/get_single_profile_bloc/get_single_profile_bloc.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/user_bloc/get_single_profile_bloc/get_single_profile_event.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/user_bloc/get_single_profile_bloc/get_single_profile_state.dart';
+import 'package:cportal_flutter/feature/presentation/ui/profile/widgets/avatar_and_userinfo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -29,14 +33,21 @@ class _ProfilePageState extends State<ProfilePage> {
         if (state is GetSingleProfileLoadedState) {
           profile = state.profile;
 
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: const Icon(
+                Icons.close,
+                color: AppColors.kLightTextColor,
+              ),
+              title: Text(
+                AppLocalizations.of(context)!.profile,
+                style: kMainTextRusso.copyWith(fontSize: 28),
+              ),
+            ),
+            body: Column(
               children: [
-                Text(profile.firstName),
-                Text(profile.middleName),
-                Text(profile.lastName),
-                Text(profile.photoLink),
+                AvatarAndUserInfo(profile: profile),
               ],
             ),
           );
