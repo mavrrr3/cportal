fbc8cb47038a136174639f858f7a949aa6629403
isoniasid <isoniasid@gmail.com>
2022-05-04 11:55:48 +0300

    delete unused imports

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cportal_flutter/common/app_colors.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const appBarTheme = AppBarTheme(
