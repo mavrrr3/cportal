bdfd038f12b8732de0f806f4f9607f670f444324
flutfix <flutfix@yandex.ru>
2022-05-12 22:49:58 +0300

    Онбординг начало

--- lib/feature/presentation/ui/home/home_page.dart
+++ lib/feature/presentation/ui/home/home_page.dart
@@ -4,9 +4,11 @@ import 'package:cportal_flutter/feature/presentation/bloc/navigation_bar_bloc/na
 import 'package:cportal_flutter/feature/presentation/bloc/navigation_bar_bloc/navigation_bar_state.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/news_bloc/fetch_news_bloc.dart';
 import 'package:cportal_flutter/feature/presentation/go_navigation.dart';
+import 'package:cportal_flutter/feature/presentation/ui/finger_print/widgets/button.dart';
 import 'package:cportal_flutter/feature/presentation/ui/home/widgets/desktop_menu.dart';
 import 'package:cportal_flutter/feature/presentation/ui/main_page/main_page.dart';
 import 'package:cportal_flutter/feature/presentation/ui/news_page/news_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -24,9 +26,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   Timer? timer;
+  late bool _isOnboarding;
   @override
   void initState() {
-    WidgetsBinding.instance.addObserver(this);
+    _isOnboarding = false;
+    WidgetsBinding.instance?.addObserver(this);
 
     super.initState();
   }
@@ -124,26 +128,115 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     return BlocBuilder<NavBarBloc, NavBarState>(
       builder: (context, state) {
         return Scaffold(
-          body: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          body: Stack(
             children: [
-              ResponsiveVisibility(
-                visible: false,
-                visibleWhen: const [
-                  Condition<dynamic>.largerThan(name: TABLET),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ResponsiveVisibility(
+                    visible: false,
+                    visibleWhen: const [
+                      Condition<dynamic>.largerThan(name: TABLET),
+                    ],
+                    child: DesktopMenu(
+                      onboarding: () {
+                        setState(
+                          () {
+                            _isOnboarding = true;
+                          },
+                        );
+                      },
+                      currentIndex: state.currentIndex,
+                      onChange: (index) {
+                        BlocProvider.of<NavBarBloc>(context)
+                            .add(NavBarEventImpl(index: index));
+                      },
+                      items: state.menuItems,
+                    ),
+                  ),
+                  Expanded(
+                    child: _listPages[state.currentIndex],
+                  ),
                 ],
-                child: DesktopMenu(
-                  currentIndex: state.currentIndex,
-                  onChange: (index) {
-                    BlocProvider.of<NavBarBloc>(context)
-                        .add(NavBarEventImpl(index: index));
-                  },
-                  items: state.menuItems,
+              ),
+              // Онбординг для Web
+              // Затемнение заднего фона
+              if (_isOnboarding)
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  color: theme.hoverColor.withOpacity(0.2),
                 ),
-              ),
-              Expanded(
-                child: _listPages[state.currentIndex],
-              ),
+
+              // Закрыть онбординг
+              if (_isOnboarding)
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 60,
+                      top: 60,
+                    ),
+                    child: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        setState(() {
+                          _isOnboarding = false;
+                        });
+                      },
+                      child: SvgPicture.asset(
+                        'assets/icons/onboarding_close.svg',
+                      ),
+                    ),
+                  ),
+                ),
+
+              // Контент онбординга и его навигация
+              if (_isOnboarding)
+                Align(
+                  alignment: Alignment.center,
+                  child: OnBoardingPopUp(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 60.0,
+                        bottom: 32,
+                        left: 32,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                AppLocalizations.of(context)!.welcome,
+                                style: theme.textTheme.headline2,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                AppLocalizations.of(context)!
+                                    .findImportantInformation,
+                                style: theme.textTheme.headline5,
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 174.0, right: 206),
+                            child: Button.factory(
+                              context,
+                              ButtonEnum.blue,
+                              AppLocalizations.of(context)!.forward,
+                              () {},
+                              const Size(double.infinity, 48),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
             ],
           ),
           bottomNavigationBar: SizedBox(
@@ -208,6 +301,66 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
           ),
         );
       },
+    );
+  }
+}
+
+class OnBoardingPopUp extends StatefulWidget {
+  const OnBoardingPopUp({
+    Key? key,
+    required this.child,
+    this.isBackArrow = false,
+    this.isNextArrow = false,
+    this.onBack,
+    this.onNext,
+  }) : super(key: key);
+
+  final bool isBackArrow;
+  final bool isNextArrow;
+  final Function()? onBack;
+  final Function()? onNext;
+  final Widget child;
+
+  @override
+  State<OnBoardingPopUp> createState() => _OnBoardingPopUpState();
+}
+
+class _OnBoardingPopUpState extends State<OnBoardingPopUp> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (widget.isBackArrow)
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: widget.onBack,
+            child: SvgPicture.asset(
+              'assets/icons/onboarding_arrow_back.svg',
+            ),
+          ),
+        const SizedBox(width: 53),
+        Container(
+          width: 844,
+          height: 601,
+          decoration: BoxDecoration(
+            color: theme.splashColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: widget.child,
+        ),
+        const SizedBox(width: 53),
+        if (widget.isNextArrow)
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: widget.onNext,
+            child: SvgPicture.asset(
+              'assets/icons/onboarding_arrow_next.svg',
+            ),
+          ),
+      ],
     );
   }
 }
