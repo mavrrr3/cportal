4ce776abb380863601a27c94503eef141601b851
flutfix <flutfix@yandex.ru>
2022-05-13 21:17:30 +0300

    Доделал онбординг

--- lib/feature/presentation/ui/home/home_page.dart
+++ lib/feature/presentation/ui/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:cportal_flutter/feature/presentation/bloc/navigation_bar_bloc/navigation_bar_bloc.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/navigation_bar_bloc/navigation_bar_event.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/navigation_bar_bloc/navigation_bar_state.dart';
@@ -8,12 +9,58 @@ import 'package:cportal_flutter/feature/presentation/ui/finger_print/widgets/but
 import 'package:cportal_flutter/feature/presentation/ui/home/widgets/desktop_menu.dart';
 import 'package:cportal_flutter/feature/presentation/ui/main_page/main_page.dart';
 import 'package:cportal_flutter/feature/presentation/ui/news_page/news_page.dart';
+import 'package:cportal_flutter/feature/presentation/ui/onboarding/onboarding_pop_up.dart';
+import 'package:cportal_flutter/feature/presentation/ui/onboarding/start_onboard.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+
+final List<OnboardingEntity> _onboardingContent = [
+  OnboardingEntity(
+    title: 'Как общаться с коллегами?',
+    description:
+        'Сегодня Вас включат в группу сотрудников Новосталь-М в WhatsApp.',
+    image: 'assets/img/onboarding/1.svg',
+  ),
+  OnboardingEntity(
+    title: 'Обязательно для ознакомления!',
+    description:
+        'Сегодня Вам будет назначен курс «Обучение по информационной безопасности», который Вам будет необходимо пройти в течение 2-х\nнедель с даты приема.',
+    image: 'assets/img/onboarding/2.svg',
+  ),
+  OnboardingEntity(
+    title: 'Любите читать?',
+    description:
+        'В ближайшее время Вы будете подключены к электронной библиотеке Компании.',
+    image: 'assets/img/onboarding/3.svg',
+  ),
+  OnboardingEntity(
+    title: 'Изучайте иностранные языки',
+    description:
+        'В нашей компании проходят курсы по английскому и испанскому языкам. Вы можете присоединиться  к коллегам, изучающих иностранные языки,  для этого Вам нужно обратиться к сотруднику HR. Обучение языкам проходит за счет компании.',
+    image: 'assets/img/onboarding/4.svg',
+  ),
+  OnboardingEntity(
+    title: 'Голодным не останетесь',
+    description: 'Компания предоставляет сотрудникам бесплатные обеды.',
+    image: 'assets/img/onboarding/5.svg',
+  ),
+  OnboardingEntity(
+    title: 'Как получить ДМС?',
+    description:
+        'После прохождения испытательного срока Вам будет оформлен полис ДМС и направлен страховой компанией на почту.',
+    image: 'assets/img/onboarding/6.png',
+    isVector: false,
+  ),
+  OnboardingEntity(
+    title: 'Всегда рады ответить на вопросы',
+    description: 'Дирекция по управлению персоналом',
+    image: 'assets/img/onboarding/7.svg',
+  ),
+];
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -24,13 +71,54 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   Timer? timer;
+  // Для онбординга
   late bool _isOnboarding;
+  late bool _isWelcome;
+  late bool _isLearningCourse;
+  late int _onBoardingIndex;
+  late PageController _pageController;
+  late AnimationController _animationController;
+  late Duration _pageDuration;
   @override
   void initState() {
+    _onBoardingIndex = 0;
+    _pageController = PageController(initialPage: _onBoardingIndex);
+    _animationController = AnimationController(vsync: this);
+    _isWelcome = false;
     _isOnboarding = false;
-    WidgetsBinding.instance?.addObserver(this);
+    _isLearningCourse = false;
+
+    _pageController = PageController(initialPage: _onBoardingIndex);
+    _animationController = AnimationController(vsync: this);
+
+    // Время показа текущей страницы
+    _pageDuration = const Duration(seconds: 5);
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.stop();
+        _animationController.reset();
+        setState(() {
+          if (_onBoardingIndex + 1 < _onboardingContent.length) {
+            _onBoardingIndex += 1;
+            _loadPage();
+          } else {
+            if (_isLearningCourse) {
+              setState(() {
+                _isLearningCourse = false;
+              });
+            } else {
+              setState(() {
+                _isOnboarding = false;
+                _isLearningCourse = true;
+              });
+            }
+          }
+        });
+      }
+    });
 
     super.initState();
   }
@@ -142,7 +230,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                       onboarding: () {
                         setState(
                           () {
-                            _isOnboarding = true;
+                            _isWelcome = true;
                           },
                         );
                       },
@@ -161,7 +249,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
               ),
               // Онбординг для Web
               // Затемнение заднего фона
-              if (_isOnboarding)
+              if (_isOnboarding || _isWelcome || _isLearningCourse)
                 Container(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height,
@@ -169,7 +257,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                 ),
 
               // Закрыть онбординг
-              if (_isOnboarding)
+              if (_isOnboarding || _isWelcome || _isLearningCourse)
                 Align(
                   alignment: Alignment.topRight,
                   child: Padding(
@@ -181,7 +269,12 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                       behavior: HitTestBehavior.translucent,
                       onTap: () {
                         setState(() {
+                          _onBoardingIndex = 0;
+                          _isWelcome = false;
                           _isOnboarding = false;
+                          _isLearningCourse = false;
+                          _animationController.stop();
+                          _animationController.reset();
                         });
                       },
                       child: SvgPicture.asset(
@@ -191,52 +284,14 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                   ),
                 ),
 
+              // Добро пожаловать
+              if (_isWelcome) _welcome(context, theme),
+
               // Контент онбординга и его навигация
-              if (_isOnboarding)
-                Align(
-                  alignment: Alignment.center,
-                  child: OnBoardingPopUp(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 60.0,
-                        bottom: 32,
-                        left: 32,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                AppLocalizations.of(context)!.welcome,
-                                style: theme.textTheme.headline2,
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                AppLocalizations.of(context)!
-                                    .findImportantInformation,
-                                style: theme.textTheme.headline5,
-                              ),
-                            ],
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 174.0, right: 206),
-                            child: Button.factory(
-                              context,
-                              ButtonEnum.blue,
-                              AppLocalizations.of(context)!.forward,
-                              () {},
-                              const Size(double.infinity, 48),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              if (_isOnboarding) _onBoarding(),
+
+              // Обучающий курс (Последний этап онбординга)
+              if (_isLearningCourse) _learningCourse(context),
             ],
           ),
           bottomNavigationBar: SizedBox(
@@ -303,64 +358,166 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
       },
     );
   }
-}
 
-class OnBoardingPopUp extends StatefulWidget {
-  const OnBoardingPopUp({
-    Key? key,
-    required this.child,
-    this.isBackArrow = false,
-    this.isNextArrow = false,
-    this.onBack,
-    this.onNext,
-  }) : super(key: key);
-
-  final bool isBackArrow;
-  final bool isNextArrow;
-  final Function()? onBack;
-  final Function()? onNext;
-  final Widget child;
-
-  @override
-  State<OnBoardingPopUp> createState() => _OnBoardingPopUpState();
-}
-
-class _OnBoardingPopUpState extends State<OnBoardingPopUp> {
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        if (widget.isBackArrow)
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: widget.onBack,
-            child: SvgPicture.asset(
-              'assets/icons/onboarding_arrow_back.svg',
-            ),
+  Widget _welcome(BuildContext context, ThemeData theme) {
+    return Align(
+      alignment: Alignment.center,
+      child: OnBoardingPopUp(
+        isBackArrow: false,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 60.0,
+            bottom: 32,
+            left: 32,
           ),
-        const SizedBox(width: 53),
-        Container(
-          width: 844,
-          height: 601,
-          decoration: BoxDecoration(
-            color: theme.splashColor,
-            borderRadius: BorderRadius.circular(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.welcome,
+                    style: theme.textTheme.headline2,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    AppLocalizations.of(context)!.findImportantInformation,
+                    style: theme.textTheme.headline5,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 174.0, right: 206),
+                child: Button.factory(
+                  context,
+                  ButtonEnum.blue,
+                  AppLocalizations.of(context)!.forward,
+                  () {
+                    setState(() {
+                      _isWelcome = false;
+                      _isOnboarding = true;
+                    });
+                    _loadPage(animateToPage: false);
+                  },
+                  const Size(double.infinity, 48),
+                ),
+              ),
+            ],
           ),
-          child: widget.child,
         ),
-        const SizedBox(width: 53),
-        if (widget.isNextArrow)
-          GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: widget.onNext,
-            child: SvgPicture.asset(
-              'assets/icons/onboarding_arrow_next.svg',
-            ),
-          ),
-      ],
+      ),
     );
+  }
+
+  Widget _onBoarding() {
+    return Align(
+      alignment: Alignment.center,
+      child: OnBoardingPopUp(
+        isNextArrow: true,
+        onNext: () {
+          setState(() {
+            // ignore: prefer-conditional-expressions
+            if (_onBoardingIndex + 1 < _onboardingContent.length) {
+              _onBoardingIndex += 1;
+              _loadPage();
+            } else {
+              setState(() {
+                _isOnboarding = false;
+                _isLearningCourse = true;
+                _animationController.stop();
+                _animationController.reset();
+                _animationController.duration = const Duration(seconds: 5);
+                _animationController.forward();
+              });
+            }
+          });
+        },
+        onBack: () {
+          setState(() {
+            if (_onBoardingIndex - 1 >= 0) {
+              _onBoardingIndex -= 1;
+              _loadPage();
+            }
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 17.0,
+            bottom: 32,
+            left: 32,
+            right: 32,
+          ),
+          child: OnBoardingContentWeb(
+            animationController: _animationController,
+            pageController: _pageController,
+            content: _onboardingContent,
+            currentIndex: _onBoardingIndex,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _learningCourse(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: OnBoardingPopUp(
+        onBack: () {
+          setState(() {
+            _isOnboarding = true;
+            _isLearningCourse = false;
+            _onBoardingIndex = _onboardingContent.length - 1;
+            _loadPage();
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 17.0,
+            bottom: 32,
+            left: 32,
+            right: 32,
+          ),
+          child: OnBoardingContentWeb(
+            animationController: _animationController,
+            pageController: _pageController,
+            isButton: true,
+            onTap: () {
+              // TODO: Отработать переход на курс
+            },
+            content: [
+              OnboardingEntity(
+                title: AppLocalizations.of(context)!.onboarding_title8,
+                description:
+                    AppLocalizations.of(context)!.onboarding_description8,
+                image: 'assets/img/onboarding/8.svg',
+              ),
+            ],
+            currentIndex: 0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _loadPage({
+    bool animateToPage = true,
+  }) {
+    _animationController.stop();
+    _animationController.reset();
+    _animationController.duration = _pageDuration;
+    _animationController.forward();
+
+    if (animateToPage) {
+      _pageController.jumpToPage(_onBoardingIndex);
+    }
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _animationController.dispose();
+    super.dispose();
   }
 }
---
+++ lib/feature/presentation/ui/onboarding/onboarding_pop_up.dart
@@ -0,0 +1,178 @@
+import 'dart:developer';
+
+import 'package:cportal_flutter/feature/presentation/go_navigation.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:cportal_flutter/feature/presentation/ui/finger_print/widgets/button.dart';
+import 'package:cportal_flutter/feature/presentation/ui/onboarding/start_onboard.dart';
+import 'package:cportal_flutter/feature/presentation/ui/onboarding/widgets/animated_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+
+class OnBoardingPopUp extends StatefulWidget {
+  const OnBoardingPopUp({
+    Key? key,
+    required this.child,
+    this.isBackArrow = true,
+    this.isNextArrow = false,
+    this.onBack,
+    this.onNext,
+  }) : super(key: key);
+
+  final bool isBackArrow;
+  final bool isNextArrow;
+  final Function()? onBack;
+  final Function()? onNext;
+  final Widget child;
+
+  @override
+  State<OnBoardingPopUp> createState() => _OnBoardingPopUpState();
+}
+
+class _OnBoardingPopUpState extends State<OnBoardingPopUp> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (widget.isBackArrow)
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: widget.onBack,
+            child: SvgPicture.asset(
+              'assets/icons/onboarding_arrow_back.svg',
+            ),
+          ),
+        const SizedBox(width: 53),
+        Container(
+          width: 844,
+          height: 601,
+          decoration: BoxDecoration(
+            color: theme.splashColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: widget.child,
+        ),
+        const SizedBox(width: 53),
+        if (widget.isNextArrow)
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: widget.onNext,
+            child: SvgPicture.asset(
+              'assets/icons/onboarding_arrow_next.svg',
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class OnBoardingContentWeb extends StatefulWidget {
+  const OnBoardingContentWeb({
+    Key? key,
+    required this.content,
+    required this.currentIndex,
+    required this.pageController,
+    required this.animationController,
+    this.isButton = false,
+    this.onTap,
+  }) : super(key: key);
+  final List<OnboardingEntity> content;
+  final int currentIndex;
+  final PageController pageController;
+  final AnimationController animationController;
+  final bool isButton;
+  final Function? onTap;
+  @override
+  State<OnBoardingContentWeb> createState() => _OnBoardingContentWebState();
+}
+
+class _OnBoardingContentWebState extends State<OnBoardingContentWeb> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Виджет прогресса
+        Row(
+          children: widget.content
+              .asMap()
+              .map((key, value) {
+                return MapEntry(
+                  key,
+                  AnimatedBar(
+                    animationController: widget.animationController,
+                    position: key,
+                    currentIndex: widget.currentIndex,
+                    height: 4,
+                  ),
+                );
+              })
+              .values
+              .toList(),
+        ),
+        SizedBox(
+          width: 855,
+          height: 541,
+          child: PageView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: widget.pageController,
+            itemCount: widget.content.length,
+            itemBuilder: (context, i) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 39),
+                Text(
+                  widget.content[i].title,
+                  style: theme.textTheme.headline2,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  widget.content[i].description,
+                  style: theme.textTheme.headline5,
+                ),
+                const SizedBox(height: 110),
+                Align(
+                  alignment: Alignment.center,
+                  child: widget.content[i].isVector
+                      ? SvgPicture.asset(
+                          widget.content[i].image,
+                          fit: BoxFit.cover,
+                        )
+                      : Image.asset(
+                          widget.content[i].image,
+                          width: 406,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                if (widget.isButton) const SizedBox(height: 32),
+                if (widget.isButton)
+                  Align(
+                    alignment: Alignment.center,
+                    child: Button.factory(
+                      context,
+                      ButtonEnum.blue,
+                      AppLocalizations.of(context)!.go_over,
+                      () {
+                        widget.onTap;
+                      },
+                      const Size(
+                        328,
+                        48,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
