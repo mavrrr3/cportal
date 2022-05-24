497347e47160747ef3004beefb11132f0c3f46b4
isoniasid <isoniasid@gmail.com>
2022-05-25 01:11:33 +0300

    сделал FilterEntity иммутабельным и изменил bloc

--- lib/feature/domain/entities/filter_entity.dart
+++ lib/feature/domain/entities/filter_entity.dart
@@ -1,30 +1,54 @@
 import 'package:equatable/equatable.dart';
 
-// ignore: must_be_immutable
 class FilterEntity extends Equatable {
   final String headline;
   final List<FilterItemEntity> items;
-  bool isActive;
+  final bool isActive;
 
-  FilterEntity({
+  const FilterEntity({
     required this.headline,
     required this.items,
     this.isActive = false,
   });
 
+  FilterEntity copyWith({
+    String? headline,
+    List<FilterItemEntity>? items,
+    bool? isActive,
+  }) {
+    return FilterEntity(
+      headline: headline ?? this.headline,
+      items: items ?? this.items,
+      isActive: isActive ?? this.isActive,
+    );
+  }
+
+  bool get changeActivity => !isActive;
+
   @override
   List<Object?> get props => [headline, items, isActive];
 }
 
-// ignore: must_be_immutable
 class FilterItemEntity extends Equatable {
   final String name;
-  bool isActive;
+  final bool isActive;
 
-  FilterItemEntity({
+  const FilterItemEntity({
     required this.name,
     this.isActive = false,
   });
+
+  FilterItemEntity copyWith({
+    String? name,
+    bool? isActive,
+  }) {
+    return FilterItemEntity(
+      name: name ?? this.name,
+      isActive: isActive ?? this.isActive,
+    );
+  }
+
+  bool get changeActivity => !isActive;
 
   @override
   List<Object?> get props => [name, isActive];
