1277d5c412f0346cd0a9309d2b7eeea243320303
flutfix <flutfix@yandex.ru>
2022-06-10 15:36:45 +0300

    Анимация на кнопку Profile pop up

--- lib/feature/presentation/ui/contacts_page/contact_profile_pop_up.dart
+++ lib/feature/presentation/ui/contacts_page/contact_profile_pop_up.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cportal_flutter/feature/presentation/bloc/get_single_profile_bloc/get_single_profile_bloc.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/get_single_profile_bloc/get_single_profile_event.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/get_single_profile_bloc/get_single_profile_state.dart';
@@ -21,8 +23,10 @@ class ContactProfilePopUp extends StatefulWidget {
 }
 
 class _ContactProfilePopUpState extends State<ContactProfilePopUp> {
+  late bool _isButtonHover;
   @override
   void initState() {
+    _isButtonHover = false;
     BlocProvider.of<GetSingleProfileBloc>(
       context,
       listen: false,
@@ -38,8 +42,8 @@ class _ContactProfilePopUpState extends State<ContactProfilePopUp> {
       builder: (context, state) {
         if (state is GetSingleProfileLoadingState) {
           return const SizedBox(
-            width: 660,
-            height: 491,
+            width: 491,
+            height: 660,
             child: Center(
               child: CircularProgressIndicator(),
             ),
@@ -47,92 +51,100 @@ class _ContactProfilePopUpState extends State<ContactProfilePopUp> {
         }
 
         if (state is GetSingleProfileLoadedState) {
-          return Stack(
-            children: [
-              Positioned(
-                right: 0,
-                child: GestureDetector(
-                  behavior: HitTestBehavior.translucent,
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.close,
-                    color: theme.hoverColor,
+          return SizedBox(
+            height: 660,
+            child: Stack(
+              children: [
+                Positioned(
+                  right: 0,
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.close,
+                      color: theme.hoverColor,
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // Profile Image & Full Name.
-                  Align(
-                    alignment: Alignment.center,
-                    child: state.profile.photoLink != ''
-                        ? AvatarBox(
-                            size: 102,
-                            imgPath: state.profile.photoLink,
-                            borderRadius: 24,
-                          )
-                        : EmptyAvatarBox(
-                            size: 102,
-                            borderRadius: 24,
-                            user: state.profile,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Profile Image & Full Name.
+                        Align(
+                          alignment: Alignment.center,
+                          child: state.profile.photoLink != ''
+                              ? AvatarBox(
+                                  size: 102,
+                                  imgPath: state.profile.photoLink,
+                                  borderRadius: 24,
+                                )
+                              : EmptyAvatarBox(
+                                  size: 102,
+                                  borderRadius: 24,
+                                  user: state.profile,
+                                ),
+                        ),
+                        const SizedBox(height: 12),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            state.profile.fullName,
+                            style: theme.textTheme.headline4!.copyWith(
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                  ),
-                  const SizedBox(height: 12),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      state.profile.fullName,
-                      style: theme.textTheme.headline4!.copyWith(
-                        fontWeight: FontWeight.w800,
-                      ),
-                      textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 32),
+                        // -- Profile info --
+                        // Post.
+                        ProfileInfoSection(
+                          headline: AppLocalizations.of(context)!.position,
+                          text: state.profile.position,
+                          bottomPadding: 18,
+                        ),
+
+                        // Department.
+                        ProfileInfoSection(
+                          headline: AppLocalizations.of(context)!.department,
+                          text: state.profile.department,
+                          bottomPadding: 18,
+                        ),
+
+                        // Contact info.
+                        ...List.generate(
+                          state.profile.contactInfo.length,
+                          (i) => ProfileInfoSection(
+                            headline: state.profile.contactInfo[i].type,
+                            text: state.profile.contactInfo[i].contact,
+                            bottomPadding: 18,
+                          ),
+                        ),
+
+                        // Birth date.
+                        if (state.profile.birthDayToString != null)
+                          ProfileInfoSection(
+                            headline: AppLocalizations.of(context)!.birth_date,
+                            text: state.profile.birthDayToString!,
+                            bottomPadding: 0,
+                          ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 32),
-                  // -- Profile info --
-                  // Post.
-                  ProfileInfoSection(
-                    headline: AppLocalizations.of(context)!.position,
-                    text: state.profile.position,
-                    bottomPadding: 18,
-                  ),
-
-                  // Department.
-                  ProfileInfoSection(
-                    headline: AppLocalizations.of(context)!.department,
-                    text: state.profile.department,
-                    bottomPadding: 18,
-                  ),
-
-                  // Contact info.
-                  ...List.generate(
-                    state.profile.contactInfo.length,
-                    (i) => ProfileInfoSection(
-                      headline: state.profile.contactInfo[i].type,
-                      text: state.profile.contactInfo[i].contact,
-                      bottomPadding: 18,
+                    _ActionButton(
+                      onTap: () {},
+                      onHover: _handleHoveHighlight,
+                      isAnimation: _isButtonHover,
                     ),
-                  ),
-
-                  // Birth date.
-                  if (state.profile.birthDayToString != null)
-                    ProfileInfoSection(
-                      headline: AppLocalizations.of(context)!.birth_date,
-                      text: state.profile.birthDayToString!,
-                      bottomPadding: 0,
-                    ),
-
-                  const SizedBox(height: 32),
-                  _ActionButton(
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           );
         }
         // TODO: Отработать другие стейты.
@@ -141,51 +153,85 @@ class _ContactProfilePopUpState extends State<ContactProfilePopUp> {
       },
     );
   }
+
+  void _handleHoveHighlight(bool value) {
+    setState(() {
+      _isButtonHover = value;
+    });
+  }
 }
 
 class _ActionButton extends StatelessWidget {
   final Function() onTap;
+  final Function(bool) onHover;
+  final bool isAnimation;
 
   // Кнопка взаимодействия с пользователем [Перейти в Чат].
   const _ActionButton({
     Key? key,
     required this.onTap,
+    required this.onHover,
+    required this.isAnimation,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: theme.primaryColor,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SvgPicture.asset(
-              'assets/icons/message.svg',
-              width: 24,
-              color: theme.brightness == Brightness.light
-                  ? theme.splashColor
-                  : theme.hoverColor,
+    return GestureDetector(
+      child: FocusableActionDetector(
+        onShowHoverHighlight: onHover,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOut,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: isAnimation ? theme.splashColor : theme.primaryColor,
+            boxShadow: isAnimation
+                ? [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      offset: const Offset(0, 4),
+                      blurRadius: 7,
+                    ),
+                  ]
+                : null,
+            border: isAnimation
+                ? Border.all(color: theme.primaryColor, width: 2)
+                : null,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/message.svg',
+                  width: 24,
+                  color: _getTextColor(theme, isAnimation),
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  AppLocalizations.of(context)!.go_to_chat,
+                  style: theme.textTheme.headline5!.copyWith(
+                    fontWeight: FontWeight.w700,
+                    color: _getTextColor(theme, isAnimation),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(width: 8),
-            Text(
-              AppLocalizations.of(context)!.go_to_chat,
-              style: theme.textTheme.headline5!.copyWith(
-                fontWeight: FontWeight.w700,
-                color: theme.brightness == Brightness.light
-                    ? theme.splashColor
-                    : theme.hoverColor,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Color _getTextColor(ThemeData theme, bool isAnimation) {
+    // ignore: prefer-conditional-expressions
+    if (theme.brightness == Brightness.light) {
+      return isAnimation ? theme.primaryColor : theme.splashColor;
+    } else {
+      return isAnimation ? theme.primaryColor : theme.hoverColor;
+    }
   }
 }
