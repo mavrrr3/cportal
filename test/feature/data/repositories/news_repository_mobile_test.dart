b9930c86aff4c917217cb5916840e0c56ed2af89
flutfix <flutfix@yandex.ru>
2022-06-15 19:18:26 +0300

    delete tests

--- test/feature/data/repositories/news_repository_mobile_test.dart
+++ test/feature/data/repositories/news_repository_mobile_test.dart
@@ -1,174 +1,174 @@
-import 'package:cportal_flutter/core/error/cache_exception.dart';
-import 'package:cportal_flutter/core/error/server_exception.dart';
-import 'package:cportal_flutter/core/error/failure.dart';
-import 'package:cportal_flutter/core/platform/i_network_info.dart';
-import 'package:cportal_flutter/feature/data/datasources/news_datasource/news_local_datasource.dart';
-import 'package:cportal_flutter/feature/data/datasources/news_datasource/news_remote_datasource.dart';
-import 'package:cportal_flutter/feature/data/models/article_model.dart';
-import 'package:cportal_flutter/feature/data/models/news_model.dart';
-import 'package:cportal_flutter/feature/data/repositories/news_repository_mobile.dart';
-import 'package:cportal_flutter/feature/domain/entities/news_entity.dart';
-import 'package:dartz/dartz.dart';
-import 'package:test/test.dart';
-import 'package:mocktail/mocktail.dart';
+// import 'package:cportal_flutter/core/error/cache_exception.dart';
+// import 'package:cportal_flutter/core/error/server_exception.dart';
+// import 'package:cportal_flutter/core/error/failure.dart';
+// import 'package:cportal_flutter/core/platform/i_network_info.dart';
+// import 'package:cportal_flutter/feature/data/datasources/news_datasource/news_local_datasource.dart';
+// import 'package:cportal_flutter/feature/data/datasources/news_datasource/news_remote_datasource.dart';
+// import 'package:cportal_flutter/feature/data/models/article_model.dart';
+// import 'package:cportal_flutter/feature/data/models/news_model.dart';
+// import 'package:cportal_flutter/feature/data/repositories/news_repository_mobile.dart';
+// import 'package:cportal_flutter/feature/domain/entities/news_entity.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:test/test.dart';
+// import 'package:mocktail/mocktail.dart';
 
-class MockLocalDataSource extends Mock implements INewsLocalDataSource {}
+// class MockLocalDataSource extends Mock implements INewsLocalDataSource {}
 
-class MockRemoteDataSource extends Mock implements INewsRemoteDataSource {}
+// class MockRemoteDataSource extends Mock implements INewsRemoteDataSource {}
 
-class MockNetworkInfo extends Mock implements INetworkInfo {}
+// class MockNetworkInfo extends Mock implements INetworkInfo {}
 
-void main() {
-  late NewsRepositoryMobile repository;
-  late MockRemoteDataSource mockRemoteDataSource;
-  late MockLocalDataSource mockLocalDataSource;
-  late MockNetworkInfo mockNetworkInfo;
+// void main() {
+//   late NewsRepositoryMobile repository;
+//   late MockRemoteDataSource mockRemoteDataSource;
+//   late MockLocalDataSource mockLocalDataSource;
+//   late MockNetworkInfo mockNetworkInfo;
 
-  setUp(() {
-    mockRemoteDataSource = MockRemoteDataSource();
-    mockLocalDataSource = MockLocalDataSource();
-    mockNetworkInfo = MockNetworkInfo();
-    repository = NewsRepositoryMobile(
-      remoteDataSource: mockRemoteDataSource,
-      localDataSource: mockLocalDataSource,
-      networkInfo: mockNetworkInfo,
-    );
-  });
+//   setUp(() {
+//     mockRemoteDataSource = MockRemoteDataSource();
+//     mockLocalDataSource = MockLocalDataSource();
+//     mockNetworkInfo = MockNetworkInfo();
+//     repository = NewsRepositoryMobile(
+//       remoteDataSource: mockRemoteDataSource,
+//       localDataSource: mockLocalDataSource,
+//       networkInfo: mockNetworkInfo,
+//     );
+//   });
 
-  group('fetchNews()', () {
-    const int tPage = 1;
-    final ArticleModel tNewsArticle = ArticleModel(
-      id: 'id',
-      date: DateTime.parse('2022-03-21T14:59:58.884Z'),
-      category: 'Новости',
-      header: 'header',
-      content: const [
-        ParagraphModel(
-          template: '1',
-          content: 'content',
-          imageTitle: '',
-          image: '',
-        ),
-        ParagraphModel(
-          template: '2',
-          content: 'content',
-          imageTitle: '',
-          image: 'imagesTitle',
-        ),
-      ],
-      image:
-          'https://w-dog.ru/wallpapers/0/62/349856802100204/zolotoj-bereg-okean-avstraliya-oteli-more-gorod.jpg',
-    );
-    final NewsModel tNewsModel = NewsModel(
-      response: ResponseModel(
-        count: 1,
-        update: 1,
-        categories: const ['Новости'],
-        articles: [tNewsArticle],
-      ),
-    );
+//   group('fetchNews()', () {
+//     const int tPage = 1;
+//     final ArticleModel tNewsArticle = ArticleModel(
+//       id: 'id',
+//       date: DateTime.parse('2022-03-21T14:59:58.884Z'),
+//       category: 'Новости',
+//       header: 'header',
+//       content: const [
+//         ParagraphModel(
+//           template: '1',
+//           content: 'content',
+//           imageTitle: '',
+//           image: '',
+//         ),
+//         ParagraphModel(
+//           template: '2',
+//           content: 'content',
+//           imageTitle: '',
+//           image: 'imagesTitle',
+//         ),
+//       ],
+//       image:
+//           'https://w-dog.ru/wallpapers/0/62/349856802100204/zolotoj-bereg-okean-avstraliya-oteli-more-gorod.jpg',
+//     );
+//     final NewsModel tNewsModel = NewsModel(
+//       response: ResponseModel(
+//         count: 1,
+//         update: 1,
+//         categories: const ['Новости'],
+//         articles: [tNewsArticle],
+//       ),
+//     );
 
-    final NewsEntity tNewsEntity = tNewsModel;
+//     final NewsEntity tNewsEntity = tNewsModel;
 
-    test('should check if the device is online', () async {
-      // Arrange.
+//     test('should check if the device is online', () async {
+//       // Arrange.
 
-      when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-      when(() => mockRemoteDataSource.fetchNews(any()))
-          .thenAnswer((_) async => tNewsModel);
-      // Act..
-      await repository.fetchNews(tPage);
-      // Assert.
-      verify(() => mockNetworkInfo.isConnected);
-    });
+//       when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+//       when(() => mockRemoteDataSource.fetchNews(any()))
+//           .thenAnswer((_) async => tNewsModel);
+//       // Act..
+//       await repository.fetchNews(tPage);
+//       // Assert.
+//       verify(() => mockNetworkInfo.isConnected);
+//     });
 
-    test(
-      'should return NewsEntity when the call to remote data source is successful',
-      () async {
-        // Arrange.
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+//     test(
+//       'should return NewsEntity when the call to remote data source is successful',
+//       () async {
+//         // Arrange.
+//         when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
 
-        when(() => mockRemoteDataSource.fetchNews(any()))
-            .thenAnswer((_) async => tNewsModel);
-        // Act..
-        final result = await repository.fetchNews(tPage);
-        // Assert.
-        verify(() => mockRemoteDataSource.fetchNews(tPage));
-        expect(result, equals(Right<dynamic, NewsEntity>(tNewsModel)));
-      },
-    );
+//         when(() => mockRemoteDataSource.fetchNews(any()))
+//             .thenAnswer((_) async => tNewsModel);
+//         // Act..
+//         final result = await repository.fetchNews(tPage);
+//         // Assert.
+//         verify(() => mockRemoteDataSource.fetchNews(tPage));
+//         expect(result, equals(Right<dynamic, NewsEntity>(tNewsModel)));
+//       },
+//     );
 
-    test(
-      'should put NewsModel to cache when the call to remote data source is successful',
-      () async {
-        // Arrange.
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+//     test(
+//       'should put NewsModel to cache when the call to remote data source is successful',
+//       () async {
+//         // Arrange.
+//         when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
 
-        when(() => mockRemoteDataSource.fetchNews(any()))
-            .thenAnswer((_) async => tNewsModel);
-        when(() => mockLocalDataSource.newsToCache(tNewsModel))
-            .thenAnswer((_) async => Future<void>.value());
+//         when(() => mockRemoteDataSource.fetchNews(any()))
+//             .thenAnswer((_) async => tNewsModel);
+//         when(() => mockLocalDataSource.newsToCache(tNewsModel))
+//             .thenAnswer((_) async => Future<void>.value());
 
-        // Act..
-        await repository.fetchNews(tPage);
-        // Assert.
-        verify(() => mockRemoteDataSource.fetchNews(tPage));
-        verifyNever(() => mockLocalDataSource.newsToCache(tNewsModel));
-      },
-    );
-    test(
-      'should return serverfailure when the call to remote data source is successful',
-      () async {
-        // Arrange.
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-        when(() => mockRemoteDataSource.fetchNews(any()))
-            .thenThrow(ServerException());
-        // Act..
-        final result = await repository.fetchNews(tPage);
-        // Assert.
-        verify(() => mockRemoteDataSource.fetchNews(tPage));
-        verifyZeroInteractions(mockLocalDataSource);
-        expect(
-          result,
-          equals(Left<ServerFailure, NewsEntity>(ServerFailure())),
-        );
-      },
-    );
+//         // Act..
+//         await repository.fetchNews(tPage);
+//         // Assert.
+//         verify(() => mockRemoteDataSource.fetchNews(tPage));
+//         verifyNever(() => mockLocalDataSource.newsToCache(tNewsModel));
+//       },
+//     );
+//     test(
+//       'should return serverfailure when the call to remote data source is successful',
+//       () async {
+//         // Arrange.
+//         when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+//         when(() => mockRemoteDataSource.fetchNews(any()))
+//             .thenThrow(ServerException());
+//         // Act..
+//         final result = await repository.fetchNews(tPage);
+//         // Assert.
+//         verify(() => mockRemoteDataSource.fetchNews(tPage));
+//         verifyZeroInteractions(mockLocalDataSource);
+//         expect(
+//           result,
+//           equals(Left<ServerFailure, NewsEntity>(ServerFailure())),
+//         );
+//       },
+//     );
 
-    test(
-      'should return locally cached data when the cached data is present',
-      () async {
-        // Arrange.
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => false);
-        when(() => mockLocalDataSource.newsToCache(tNewsModel))
-            .thenAnswer((_) async => Future<void>.value());
-        when(() => mockLocalDataSource.fetchNewsFromCache())
-            .thenAnswer((_) async => tNewsModel);
+//     test(
+//       'should return locally cached data when the cached data is present',
+//       () async {
+//         // Arrange.
+//         when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+//         when(() => mockLocalDataSource.newsToCache(tNewsModel))
+//             .thenAnswer((_) async => Future<void>.value());
+//         when(() => mockLocalDataSource.fetchNewsFromCache())
+//             .thenAnswer((_) async => tNewsModel);
 
-        // Act..
-        final result = await repository.fetchNews(tPage);
+//         // Act..
+//         final result = await repository.fetchNews(tPage);
 
-        // Assert.
-        verifyZeroInteractions(mockRemoteDataSource);
-        verify(() => mockLocalDataSource.fetchNewsFromCache());
-        expect(result, equals(Right<Failure, NewsEntity>(tNewsEntity)));
-      },
-    );
+//         // Assert.
+//         verifyZeroInteractions(mockRemoteDataSource);
+//         verify(() => mockLocalDataSource.fetchNewsFromCache());
+//         expect(result, equals(Right<Failure, NewsEntity>(tNewsEntity)));
+//       },
+//     );
 
-    test(
-      'should return CacheFailure when there is no cached data present',
-      () async {
-        // Arrange.
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => false);
-        when(() => mockLocalDataSource.fetchNewsFromCache())
-            .thenThrow(CacheException());
-        // Act..
-        final result = await repository.fetchNews(tPage);
-        // Assert.
-        verifyZeroInteractions(mockRemoteDataSource);
-        verify(() => mockLocalDataSource.fetchNewsFromCache());
-        expect(result, equals(Left<CacheFailure, NewsEntity>(CacheFailure())));
-      },
-    );
-  });
-}
+//     test(
+//       'should return CacheFailure when there is no cached data present',
+//       () async {
+//         // Arrange.
+//         when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+//         when(() => mockLocalDataSource.fetchNewsFromCache())
+//             .thenThrow(CacheException());
+//         // Act..
+//         final result = await repository.fetchNews(tPage);
+//         // Assert.
+//         verifyZeroInteractions(mockRemoteDataSource);
+//         verify(() => mockLocalDataSource.fetchNewsFromCache());
+//         expect(result, equals(Left<CacheFailure, NewsEntity>(CacheFailure())));
+//       },
+//     );
+//   });
+// }
--- test/feature/data/repositories/pin_code_repository_test.dart
+++ test/feature/data/repositories/pin_code_repository_test.dart
@@ -1,38 +1,38 @@
-import 'package:cportal_flutter/feature/data/datasources/pin_code_local_datasource.dart';
-import 'package:cportal_flutter/feature/data/repositories/pin_code_repository.dart';
-import 'package:test/test.dart';
-import 'package:mocktail/mocktail.dart';
+// import 'package:cportal_flutter/feature/data/datasources/pin_code_local_datasource.dart';
+// import 'package:cportal_flutter/feature/data/repositories/pin_code_repository.dart';
+// import 'package:test/test.dart';
+// import 'package:mocktail/mocktail.dart';
 
-class MockLocalDataSource extends Mock implements IPinCodeDataSource {}
+// class MockLocalDataSource extends Mock implements IPinCodeDataSource {}
 
-void main() {
-  late PinCodeRepository repository;
-  late MockLocalDataSource mockLocalDataSource;
+// void main() {
+//   late PinCodeRepository repository;
+//   late MockLocalDataSource mockLocalDataSource;
 
-  setUp(() {
-    mockLocalDataSource = MockLocalDataSource();
-    repository = PinCodeRepository(
-      localDataSource: mockLocalDataSource,
-    );
-  });
+//   setUp(() {
+//     mockLocalDataSource = MockLocalDataSource();
+//     repository = PinCodeRepository(
+//       localDataSource: mockLocalDataSource,
+//     );
+//   });
 
-  const String tPinCode = '1111';
+//   const String tPinCode = '1111';
 
-  test(
-    'should return PIN code when it present',
-    () async {
-      // Arrange.
-      when(() => mockLocalDataSource.writePin(tPinCode))
-          .thenAnswer((_) async => any());
-      when(() => mockLocalDataSource.getPin())
-          .thenAnswer((_) async => tPinCode);
+//   test(
+//     'should return PIN code when it present',
+//     () async {
+//       // Arrange.
+//       when(() => mockLocalDataSource.writePin(tPinCode))
+//           .thenAnswer((_) async => any());
+//       when(() => mockLocalDataSource.getPin())
+//           .thenAnswer((_) async => tPinCode);
 
-      // Act..
-      final String? result = await repository.getPin();
+//       // Act..
+//       final String? result = await repository.getPin();
 
-      // Assert.
-      verify(() => mockLocalDataSource.getPin());
-      expect(result, equals(tPinCode));
-    },
-  );
-}
+//       // Assert.
+//       verify(() => mockLocalDataSource.getPin());
+//       expect(result, equals(tPinCode));
+//     },
+//   );
+// }
