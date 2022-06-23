9d2a8d13e897b9ed1f2efed3f75b91620dc12171
isoniasid <isoniasid@gmail.com>
2022-06-23 12:20:18 +0300

    добавил backArrow в константы

--- lib/common/constants/image_assets.dart
+++ lib/common/constants/image_assets.dart
@@ -1,4 +1,5 @@
 abstract class ImageAssets {
   static const splash = 'assets/img/bg_splash.png';
   static const document = 'assets/icons/what_get_icon.svg';
+  static const backArrow = 'assets/icons/back_arrow.svg';
 }
