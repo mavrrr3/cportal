71d44dfc03bffc64b03b51470b31c5dc2de786d8
isoniasid <isoniasid@gmail.com>
2022-06-23 08:32:17 +0300

    вынес новости с главное для веб в отдельный виджет

---
+++ lib/feature/presentation/ui/main_page/widgets/news_main_web.dart
@@ -0,0 +1,34 @@
+import 'package:cportal_flutter/common/util/padding.dart';
+import 'package:cportal_flutter/feature/domain/entities/article_entity.dart';
+import 'package:cportal_flutter/feature/presentation/navigation_route_names.dart';
+import 'package:cportal_flutter/feature/presentation/ui/widgets/news_card_item.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NewsMainWeb extends StatelessWidget {
+  final List<ArticleEntity> articles;
+  const NewsMainWeb({Key? key, required this.articles}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: getHorizontalPadding(context),
+      child: Wrap(
+        spacing: 16,
+        runSpacing: 20,
+        children: List.generate(
+          articles.length,
+          (i) => NewsCardItem(
+            onTap: () => GoRouter.of(context).pushNamed(
+              NavigationRouteNames.newsArticlePage,
+              params: {'fid': articles[i].id},
+            ),
+            width: 312,
+            height: 152,
+            item: articles[i],
+          ),
+        ),
+      ),
+    );
+  }
+}
