b037af962287d9aeecb6b89c235fef0163946c0e
isoniasid <isoniasid@gmail.com>
2022-06-24 10:46:15 +0300

    разделил newscarditem на два виджета

---
+++ lib/feature/presentation/ui/news_page/widgets/news_card.dart
@@ -0,0 +1,32 @@
+import 'package:cportal_flutter/feature/domain/entities/article_entity.dart';
+import 'package:cportal_flutter/feature/presentation/ui/news_page/widgets/news_card_item.dart';
+import 'package:flutter/material.dart';
+
+class NewsCard extends StatelessWidget {
+  final double width;
+  final ArticleEntity item;
+  final Function() onTap;
+  const NewsCard(
+    this.width, {
+    Key? key,
+    required this.item,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: onTap,
+        child: NewsCardItem(
+          width: width,
+          height: 160,
+          fontSize: 17,
+          item: item,
+        ),
+      ),
+    );
+  }
+}
