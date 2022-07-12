9f7110d7968f328fce564d4a36690e1cb815211e
flutfix <flutfix@yandex.ru>
2022-07-12 11:59:34 +0300

    отклик на кнопки в контактах

---
+++ lib/common/constants/uri_schemes.dart
@@ -0,0 +1,5 @@
+class UriSchemes {
+  static const tel = 'tel';
+  static const sms = 'sms';
+  static const mail = 'mailto';
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import device_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
 import smart_auth
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
