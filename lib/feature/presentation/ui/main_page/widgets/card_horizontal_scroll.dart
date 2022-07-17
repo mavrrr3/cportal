677297711e771c6a64d18cfba290bbc773555328
isoniasid <isoniasid@gmail.com>
2022-07-17 13:13:06 +0300

    add onboarding redirect on main top cards

--- lib/feature/presentation/ui/main_page/widgets/card_horizontal_scroll.dart
+++ lib/feature/presentation/ui/main_page/widgets/card_horizontal_scroll.dart
@@ -1,6 +1,8 @@
 import 'package:cportal_flutter/common/custom_theme.dart';
+import 'package:cportal_flutter/feature/presentation/navigation_route_names.dart';
 import 'package:cportal_flutter/feature/presentation/ui/widgets/menu/on_hover.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CardHorizontalScroll extends StatelessWidget {
   final IconData icon;
@@ -20,29 +22,32 @@ class CardHorizontalScroll extends StatelessWidget {
 
     return OnHover(
       builder: (isHovered) {
-        return Container(
-          width: 148,
-          height: 92,
-          decoration: BoxDecoration(
-            color: isHovered ? color?.withOpacity(0.6) : color,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(
-                  icon,
-                  size: 20,
-                  color: theme.textLight,
-                ),
-                Text(
-                  text,
-                  style: theme.textTheme.px14.copyWith(height: 1.5),
-                ),
-              ],
+        return GestureDetector(
+          onTap: () => context.goNamed(NavigationRouteNames.onBoardingStart),
+          child: Container(
+            width: 148,
+            height: 92,
+            decoration: BoxDecoration(
+              color: isHovered ? color?.withOpacity(0.6) : color,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    icon,
+                    size: 20,
+                    color: theme.textLight,
+                  ),
+                  Text(
+                    text,
+                    style: theme.textTheme.px14.copyWith(height: 1.5),
+                  ),
+                ],
+              ),
             ),
           ),
         );
