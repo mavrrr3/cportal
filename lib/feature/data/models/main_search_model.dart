837f44746a1f2f4287ee5138336249b092e0465f
isoniasid <isoniasid@gmail.com>
2022-07-20 21:04:01 +0300

    edit MainSearchModel

--- lib/feature/data/models/main_search_model.dart
+++ lib/feature/data/models/main_search_model.dart
@@ -1,28 +1,39 @@
 import 'package:cportal_flutter/feature/domain/entities/main_search_entity.dart';
-import 'package:json_annotation/json_annotation.dart';
 
-part 'main_search_model.g.dart';
-
-@JsonSerializable(createToJson: false)
-class MainSearchModel {
-  final String category;
-  final String title;
-  final String id;
-
-  MainSearchModel({
-    required this.category,
-    required this.title,
-    required this.id,
-  });
+class MainSearchModel extends MainSearchEntity {
+  const MainSearchModel({
+    required String category,
+    required String id,
+    required String title,
+  }) : super(
+          category: category,
+          id: id,
+          title: title,
+        );
 
   factory MainSearchModel.fromJson(Map<String, dynamic> json) =>
-      _$MainSearchModelFromJson(json);
+      MainSearchModel(
+        category: json['category'] as String,
+        id: json['id'] as String,
+        title: json['title'] as String,
+      );
+}
 
-  MainSearchEntity toEntity() {
-    return MainSearchEntity(
-      id: id,
-      category: category,
-      title: title,
+class MainSearchDeserializeModel {
+  final List<MainSearchModel> searchList;
+
+  const MainSearchDeserializeModel({
+    required this.searchList,
+  });
+
+  factory MainSearchDeserializeModel.fromJson(Map<String, dynamic> json) {
+    return MainSearchDeserializeModel(
+      searchList: json['response']['items'] != null
+          ? List<MainSearchModel>.from(json['response']['items'].map(
+              (dynamic x) =>
+                  MainSearchModel.fromJson(x as Map<String, dynamic>),
+            ) as Iterable<dynamic>)
+          : [],
     );
   }
 }
