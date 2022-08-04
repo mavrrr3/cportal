f973f2424e830125a7bdb3d8a08ccf2ca965c106
isoniasid <isoniasid@gmail.com>
2022-08-04 19:29:28 +0300

    fix visible area of news on mainPage

--- lib/feature/presentation/ui/widgets/news_main_mobile.dart
+++ lib/feature/presentation/ui/widgets/news_main_mobile.dart
@@ -1,4 +1,3 @@
-import 'package:cportal_flutter/common/util/padding.dart';
 import 'package:cportal_flutter/feature/domain/entities/article_entity.dart';
 import 'package:cportal_flutter/feature/presentation/navigation/navigation_route_names.dart';
 import 'package:cportal_flutter/feature/presentation/ui/widgets/news/news_card_similar_item.dart';
@@ -17,43 +16,34 @@ class NewsMainMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: getSingleHorizontalPadding(
-          context,
-        ),
-      ),
-      child: SizedBox(
-        height: 220,
-        width: double.infinity,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          physics: const BouncingScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: articles.length,
-          itemBuilder: (context, i) {
-            return Row(
-              children: [
-                /// Условие, чтобы не отрисовывалась новость, которая сейчас открыта.
-                if (articles[i] != currentArticle)
-                  Padding(
-                    padding: EdgeInsets.only(
-                      right: i != articles.length - 1
-                          ? 16
-                          : getSingleHorizontalPadding(context),
-                    ),
-                    child: NewsCardSimilarItem(
-                      item: articles[i],
-                      onTap: () => GoRouter.of(context).pushNamed(
-                        NavigationRouteNames.newsArticlePage,
-                        params: {'fid': articles[i].id},
-                      ),
+    return SizedBox(
+      height: 220,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: articles.length,
+        itemBuilder: (context, i) {
+          return Row(
+            children: [
+              /// Условие, чтобы не отрисовывалась новость, которая сейчас открыта.
+              if (articles[i] != currentArticle)
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                  ),
+                  child: NewsCardSimilarItem(
+                    item: articles[i],
+                    onTap: () => GoRouter.of(context).pushNamed(
+                      NavigationRouteNames.newsArticlePage,
+                      params: {'fid': articles[i].id},
                     ),
                   ),
-              ],
-            );
-          },
-        ),
+                ),
+            ],
+          );
+        },
       ),
     );
   }
