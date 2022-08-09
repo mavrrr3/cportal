5dd3ecf51db9f79b49c9a8ccf00eeee10e0eece9
isoniasid <isoniasid@gmail.com>
2022-08-09 11:03:38 +0300

    single article UI fix

---
+++ lib/feature/presentation/ui/news_page/articles/widgets/single_news_mobile_appbar.dart
@@ -0,0 +1,60 @@
+import 'package:cportal_flutter/app_config.dart';
+import 'package:cportal_flutter/common/theme/custom_theme.dart';
+import 'package:cportal_flutter/feature/domain/entities/article_entity.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SingleNewsMobileAppbar extends StatelessWidget {
+  final ArticleEntity article;
+
+  const SingleNewsMobileAppbar({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final CustomTheme theme = Theme.of(context).extension<CustomTheme>()!;
+
+    return SliverAppBar(
+      backgroundColor: theme.background,
+      expandedHeight: 176,
+      automaticallyImplyLeading: false,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Stack(
+          fit: StackFit.expand,
+          children: [
+            ExtendedImage.network(
+              '${AppConfig.imagesUrl}/${article.image}',
+              fit: BoxFit.cover,
+              cache: true,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: theme.black!.withOpacity(0.42),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    enableFeedback: false,
+                    icon: Icon(Icons.arrow_back, color: theme.white),
+                    iconSize: 26,
+                    onPressed: () => context.pop(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
