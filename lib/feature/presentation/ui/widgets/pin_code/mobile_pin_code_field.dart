46225dd13e36b3dcefeeda5561c651b4e333acf4
flutfix <flutfix@yandex.ru>
2022-08-25 19:47:05 +0400

    fix

--- lib/feature/presentation/ui/widgets/pin_code/mobile_pin_code_field.dart
+++ lib/feature/presentation/ui/widgets/pin_code/mobile_pin_code_field.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: prefer_int_literals
 
 import 'package:cportal_flutter/common/theme/custom_theme.dart';
-import 'package:cportal_flutter/feature/presentation/ui/widgets/pin_code/mobile_animations/pin_code_fill_animation.dart';
-import 'package:cportal_flutter/feature/presentation/ui/widgets/pin_code/mobile_animations/pin_code_input_animation.dart';
 import 'package:cportal_flutter/feature/presentation/ui/widgets/pin_code/pin_dot.dart';
 import 'package:flutter/material.dart';
 
@@ -38,29 +36,6 @@ class _MobilePinCodeFieldState extends State<MobilePinCodeField> {
         const SizedBox(width: 32),
         PinDot(controller: widget.controller, symbolIndex: 4),
       ],
-    );
-  }
-
-  Widget _buildDot({
-    required AnimationController scaleController,
-    required AnimationController colorController,
-  }) {
-    return AnimatedBuilder(
-      animation: PinCodeFillAnimation(colorController).boxColor,
-      builder: (context, _) {
-        return ScaleTransition(
-          scale: PinCodeInputAnimation(scaleController).boxScaleIn,
-          child: AnimatedContainer(
-            width: 16,
-            height: 16,
-            duration: const Duration(milliseconds: 150),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: PinCodeFillAnimation(colorController).boxColor.value,
-            ),
-          ),
-        );
-      },
     );
   }
 }
