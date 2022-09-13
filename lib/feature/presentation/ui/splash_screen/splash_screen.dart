b54d7be4eb78c8e451b58e0f3d4d192bcc0d91b7
isoniasid <isoniasid@gmail.com>
2022-09-13 21:09:59 +0300

    fix new employee bug

--- lib/feature/presentation/ui/splash_screen/splash_screen.dart
+++ lib/feature/presentation/ui/splash_screen/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'package:cportal_flutter/feature/presentation/bloc/auth_bloc/auth_bloc.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/auth_bloc/auth_event.dart';
 import 'package:cportal_flutter/feature/presentation/bloc/auth_bloc/auth_state.dart';
-import 'package:cportal_flutter/feature/presentation/bloc/news_bloc/fetch_news_bloc.dart';
-import 'package:cportal_flutter/feature/presentation/bloc/questions_bloc/fetch_questions_bloc.dart';
 import 'package:cportal_flutter/feature/presentation/navigation/navigation_route_names.dart';
 import 'package:cportal_flutter/feature/presentation/ui/widgets/splash_widget.dart';
 import 'package:flutter/material.dart';
@@ -20,24 +18,23 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     context.read<AuthBloc>().add(const CheckLogin());
-    _fetchContent();
+    // _fetchContent();.
     super.initState();
   }
 
-  void _fetchContent() {
-    context
-      ..read<FetchNewsBloc>().add(const FetchAllNewsEvent())
-      ..read<FetchQuestionsBloc>().add(const FetchQuestionsEvent());
-  }
+  // void _fetchContent() {
+  //   context
+  //     ..read<FetchNewsBloc>().add(const FetchAllNewsEvent())
+  //     ..read<FetchQuestionsBloc>().add(const FetchQuestionsEvent());
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocListener<AuthBloc, AuthState>(
         listener: (context, state) {
-          final nextScreen = state is HasAuthCredentials
-              ? NavigationRouteNames.login
-              : NavigationRouteNames.connectingCode;
+          final nextScreen =
+              state is HasAuthCredentials ? NavigationRouteNames.login : NavigationRouteNames.connectingCode;
 
           context.goNamed(nextScreen);
         },
